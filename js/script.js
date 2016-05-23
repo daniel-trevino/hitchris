8c50515635f3fe84dd310c772eaa56e7bd63d708
Christian Österlind <christian.osterlind@occident.se>
2016-05-23 17:12:15 +0200

    some easy pie chart stuff and more

--- js/script.js
+++ js/script.js
@@ -78,6 +78,18 @@ $(document).ready(function() {
 		} 
 	});
 
+
+// SHowcase
+
+    $('.chart').easyPieChart({
+        scaleColor: "#ecf0f1",
+	    lineWidth: 20,
+	    lineCap: 'butt',
+	    barColor: '#f94949',
+	    trackColor:	"#ecf0f1",
+	    size: 160,
+	    animate: 1500
+    });
 	
 	
 	
